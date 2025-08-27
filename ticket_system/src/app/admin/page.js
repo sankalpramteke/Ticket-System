bbdb139b39490dbf1f2012c248cf4a21c7070459
sankalpramteke <116703076+sankalpramteke@users.noreply.github.com>
2025-08-27 16:47:05 +0530

    versal

--- ticket_system/src/app/admin/page.js
+++ ticket_system/src/app/admin/page.js
@@ -1,13 +1,10 @@
-import { Suspense } from 'react'
-import AdminPageClient from './AdminPageClient'
+import dynamic from 'next/dynamic'
+const AdminPageClient = dynamic(() => import('./AdminPageClient'), { ssr: false })
 
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
+export const revalidate = 0
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div className="text-gray-600">Loading...</div>}>
-      <AdminPageClient />
-    </Suspense>
-  )
+  return <AdminPageClient />
 }
